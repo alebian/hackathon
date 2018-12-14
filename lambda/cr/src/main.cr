457ebfd6362313817d5d2290c508013b5967984f
ilanusse <inaki.lanusse@increasecard.com>
2018-12-14 14:58:17 -0300

    Crystal Lambda up and running

--- lambda/cr/src/main.cr
+++ lambda/cr/src/main.cr
@@ -3,7 +3,7 @@ require "crambda"
 
 def handler(event : JSON::Any, context : Crambda::Context)
   pp context
-  pp "Ale gil"
+  { statusCode: 200, headers: { "Content-Type" => "application/json" }, body: { ale: "gil" }.to_json }
 end
 
 Crambda.run_handler(->handler(JSON::Any, Crambda::Context))
